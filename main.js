ee82f0c51f9278b462a48fd32156a212a6fa1a3b
ToSaturn <saveleva_ol@mail.ru>
2019-03-04 00:48:26 +0300

    not finished

---
+++ main.js
@@ -0,0 +1,17 @@
+// function fetchResults(searchQuery) {
+//     const endpoint = `https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+//     fetch(endpoint)
+//       .then(response => response.json())
+//       .then(data => {
+//         const results = data.query.search;
+//         console.log(results)
+//      <p id="text">${json.quoteText}</p>
+//     });
+//   }
+//   function search()
+// {
+// var searchInput = document.getElementById('search').text();
+// var url = "https://ru.wikipedia.org/w/index.php?search=" + searchInput ;
+// window.open(url);
+// }
+
